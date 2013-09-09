aa30bcb5c69bb89c75c8ac72877cfcc0450a7b3e
Loic Ortola <loic.ortola@cpe.fr>
2013-09-09 14:51:55 +0200

    added user dao

---
+++ UserDao.java
@@ -0,0 +1,54 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class UserDao {
+	private static final String DB_URL="jdbc:mysql://127.0.0.1:3306/test-epf";
+	private static final String USER="admin";
+	private static final String PASS="admin";
+	
+	public static void main(String[] args) {
+		Connection conn = null;
+		Statement stmt = null;
+		   try{
+		      //STEP 2: Register JDBC driver
+		      Class.forName("com.mysql.jdbc.Driver");
+
+		      //STEP 3: Open a connection
+		      System.out.println("Connecting to database...");
+		      conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+		      //STEP 4: Execute a query
+		      System.out.println("Creating statement...");
+		      stmt = conn.createStatement();
+		      String sql;
+		      sql = "SELECT id, first, last, age FROM Employees";
+		      ResultSet rs = stmt.executeQuery(sql);
+
+		      //STEP 5: Extract data from result set
+		      while(rs.next()){
+		         //Retrieve by column name
+		         int id  = rs.getInt("id");
+		         int age = rs.getInt("age");
+		         String first = rs.getString("first");
+		         String last = rs.getString("last");
+
+		         //Display values
+		         System.out.print("ID: " + id);
+		         System.out.print(", Age: " + age);
+		         System.out.print(", First: " + first);
+		         System.out.println(", Last: " + last);
+		      }
+		      //STEP 6: Clean-up environment
+		      rs.close();
+		      stmt.close();
+		      conn.close();
+		   }catch(SQLException se){
+		      //Handle errors for JDBC
+	}
+	
+}
+
